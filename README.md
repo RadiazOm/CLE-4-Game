2fc4d9c6a81747c0c7a3ccf52476ef3befb65dfe
RadiazOm <jeffrey.otterloo@gmail.com>
2023-06-14 15:15:04 +0200

    Readme install guide and startcode

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CLE4 main game
 
-nothing yet, will come in the future :O
+# How to get the project in your IDE (visual studio code)
+
+- step 1: clone this repository
+- step 2: open a new terminal and type "npm install"
+- step 3: after it is done loading type "npm install excalibur"
+
+and then you should be ready to go
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,25 @@
+import { DisplayMode, Engine } from "excalibur"
+import { ResourceLoader } from "./loader";
 
 
+export class Game extends Engine {
+    constructor() {
+        // The width and height will be in a 16:9 format, this is suvject to change
+        // maxFps and displaymode are for performance and sizing requirements
+        super({
+            width: 1600,
+            height: 900,
+            maxFps: 144,
+            displayMode: DisplayMode.FitScreen
+        });
+        // Antialiasing set to false otherwise pixelart will look blurry
+        this.setAntialiasing(false)
+        // If something isnt going so well you can turn this to true and you will be able to see all sorts of cool information
+        this.showDebug(false)
+        this.start(ResourceLoader).then(() => this.startGame());
+    }
 
-export class Game extends Engine
+    startGame() {
+        // TODO: Scene manager
+    }
+}
