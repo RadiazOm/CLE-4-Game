cfcff8030fa4141f98f4c5871cfb3007260979b2
RadiazOm <jeffrey.otterloo@gmail.com>
2023-06-16 11:44:43 +0200

    main back to normal

--- src/js/main.js
+++ src/js/main.js
@@ -30,24 +30,6 @@ export class Game extends Engine {
         console.log('yes')
         this.mainController = new MainController()
         this.add(this.mainController)
-
-        this.testActor = new Actor({
-            pos: new Vector(this.screen.drawWidth / 2, this.screen.drawHeight / 2)
-        })
-        this.testActor.graphics.use(Resources.Snowboard.toSprite())
-        this.add(this.testActor)
-    }
-
-    onPostUpdate() {
-        if (this.mainController.player1 != null) {
-            this.testActor.vel.x = this.mainController.player1.Axis.x * 100
-            this.testActor.vel.y = this.mainController.player1.Axis.y * 100
-
-        }
-    }
-
-    Button5(player) {
-        console.log('button5 ' + player)
     }
 }
 
