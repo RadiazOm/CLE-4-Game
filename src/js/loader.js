7ef23bfc8d292a3369d38e650df681397472e097
JordanGoedbloed <jr.goedbloed@gmail.com>
2023-06-19 09:39:48 +0200

    added background and own game file

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,15 +1,18 @@
 import { ImageSource, Sound, Resource, Loader , Color} from "excalibur";
 import dinoImage from "../images/dino.jpg";
 import skiImage from "../images/Snowboarder.png"
+import afvalImage from "../images/backgrounds/backgroundAfval.png"
 
 
 const Resources = {
   Dino: new ImageSource(dinoImage),
-  Snowboard: new ImageSource(skiImage)
+  Snowboard: new ImageSource(skiImage),
+  BackgroundAfval: new ImageSource(afvalImage)
 };
 const ResourceLoader = new Loader([
   Resources.Dino,
-  Resources.Snowboard
+  Resources.Snowboard,
+  Resources.BackgroundAfval
 ]);
 
 // ResourceLoader.logo = titleImage
