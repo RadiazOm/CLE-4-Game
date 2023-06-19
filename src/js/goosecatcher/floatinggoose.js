40775e5bbe75988cebb1a7f9134ec56840630115
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-06-19 12:30:02 +0300

    goose random place

--- src/js/goosecatcher/floatinggoose.js
+++ src/js/goosecatcher/floatinggoose.js
@@ -1,18 +1,29 @@
-import { Actor, Vector } from "excalibur";
+import { Actor, Vector, clamp } from "excalibur";
 import { Resources } from "../loader";
 
 export class GooseFloating extends Actor {
     constructor() {
-        super();
+        super({
+            width: Resources.GooseFloating.width,
+            height: Resources.GooseFloating.height,
+        });
     }
 
     onInitialize(engine) {
+        this.engine = engine
         this.graphics.use(Resources.GooseFloating.toSprite());
-        this.pos = new Vector(160, 90);
-
-        
+        this.pos.x = Math.random()* this.engine.screen.drawWidth
+        if(this.pos.x < 225 && this.pos.x >135){
+            this.pos.y = Math.random()* this.engine.screen.drawHeight
+        } else {
+            this.pos.y = (Math.random()*15) + (this.engine.screen.drawHeight - 5 ) / 2
+        }    
     }
 
-    
-
+    onPostUpdate(){
+        this.pos.x = clamp(this.pos.x, 0 + Resources.GooseFloating.width / 2, this.engine.screen.drawWidth - Resources.GooseFloating.width / 2)
+        this.pos.y = clamp(this.pos.y, 0 + Resources.GooseFloating.height / 2, this.engine.screen.drawHeight - Resources.GooseFloating.height / 2)
+    }
 }
+
+
