d2d8374c8661100d961116981eedfe637b99df1b
Jaden Birginia <jaden.birginia@gmail.com>
2023-06-21 13:32:50 +0200

    Mini game bijna klaar andere soorten ganzen zijn nodig

--- src/js/control.js
+++ src/js/control.js
@@ -2,20 +2,34 @@ import { Actor, Vector, Input } from "excalibur"
 
 export class GansWit extends Actor {
 
-    constructor(){
+    player
+
+    constructor(player){
         super()
+        this.player = player
     }
 
     onInitialize(engine) {
         let kb = engine.input.keyboard
 
-        engine.screen.drawWidth
         kb.on("press", (event) => {
             console.log(event)
             if (event.key == "KeyW") {
-                this.pos.x = this.pos.x + 10  
+                this.pos.x = this.pos.x + 5  
             }
         })
     }
 
+    onPreUpdate() {
+        console.log(this.pos.x)
+        if (this.pos.x > 325) {
+            this.engine.gameEnd()
+        }
+    }
+
+    press() {
+        this.pos.x += 5
+    }
+
 }
+//325 is de max
